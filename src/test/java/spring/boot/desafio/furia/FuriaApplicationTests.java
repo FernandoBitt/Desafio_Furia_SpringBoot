167a2a15eb1e62b73e3c412023025018dad27e4a
Fernando Santos <f.bittencourtdossantos@gmail.com>
2025-04-28 20:23:36 -0300

    Primeiro Commit

---
+++ src/test/java/spring/boot/desafio/furia/FuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.furia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
