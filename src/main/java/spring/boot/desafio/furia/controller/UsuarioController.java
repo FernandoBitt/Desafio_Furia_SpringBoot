8425c9adf0eb018af2a9e38ed5d6db41335ff712
Fernando Santos <f.bittencourtdossantos@gmail.com>
2025-04-30 20:12:40 -0300

    Adcionado Documentação Swagger url: http://localhost:8080/swagger-ui.html

--- src/main/java/spring/boot/desafio/furia/controller/UsuarioController.java
+++ src/main/java/spring/boot/desafio/furia/controller/UsuarioController.java
@@ -3,25 +3,36 @@ package spring.boot.desafio.furia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import spring.boot.desafio.furia.dto.UsuarioDTO;
 import spring.boot.desafio.furia.model.Usuario;
 import spring.boot.desafio.furia.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "http://localhost:4200") // Permite requisições do Angular
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> salvar(@RequestBody UsuarioDTO dto){
-        Usuario usuarioSalvo = usuarioService.salvarUsuario(dto);
+    @Operation(description = "Endpoint responsável por salvar dados no BD")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Dados Inseridos"),
+        @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    })
+
+    public ResponseEntity<Usuario> salvar(@RequestBody UsuarioDTO usuarioDTO){
+        Usuario usuarioSalvo = usuarioService.salvarUsuario(usuarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
     }
 
